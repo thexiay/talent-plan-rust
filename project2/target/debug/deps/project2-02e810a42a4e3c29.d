a09233afb3a92f2f21cba3ba253fe7cc91dd8d02
thexia <xiayu1187@gmail.com>
2023-11-18 16:04:43 +0800

    delete project2 info

--- project2/target/debug/deps/project2-02e810a42a4e3c29.d
+++
@@ -1,5 +0,0 @@
-/data/gitcode/talent-plan-rust/project2/target/debug/deps/project2-02e810a42a4e3c29.rmeta: src/lib.rs
-
-/data/gitcode/talent-plan-rust/project2/target/debug/deps/project2-02e810a42a4e3c29.d: src/lib.rs
-
-src/lib.rs:
--- project2/target/debug/deps/project2-a32a6d2527096708.d
+++
@@ -1,5 +0,0 @@
-/data/gitcode/talent-plan-rust/project2/target/debug/deps/project2-a32a6d2527096708.rmeta: src/lib.rs
-
-/data/gitcode/talent-plan-rust/project2/target/debug/deps/project2-a32a6d2527096708.d: src/lib.rs
-
-src/lib.rs:
