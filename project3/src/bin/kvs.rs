fb05bc2eeb44caec0674b0c85e2cf952fce42089
thexia <xiayu1187@gmail.com>
2023-11-24 18:42:37 +0800

    init project3

---
+++ project3/src/bin/kvs.rs
@@ -0,0 +1,51 @@
+#![feature(let_chains)]
+
+use std::path::Path;
+use std::process::exit;
+
+use clap::{Parser, Subcommand};
+use kvs::ErrorCode;
+use kvs::KvError;
+use kvs::KvStore;
+use kvs::Result;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Opts {
+    #[command(subcommand)]
+    command: Commands,
+    // 支持子命令: get ,set,remove
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Get { key: String },
+    Set { key: String, value: String },
+    Rm { key: String },
+}
+
+fn main() -> Result<()> {
+
+    let opts = Opts::parse();
+    let mut kv_store = KvStore::open(&std::env::current_dir()?)?;
+    match opts.command {
+        Commands::Get { key } => {
+            match kv_store.get(key)? {
+                Some(value) => println!("{}", value),
+                None => println!("Key not found")
+            }
+            Ok(())
+        }
+        Commands::Set { key, value } => {
+            kv_store.set(key, value)?;
+            Ok(())
+        }
+        Commands::Rm { key} => {
+            let res = kv_store.remove(key);
+            if let Err(error) = &res && let ErrorCode::RmError(_) = **error {
+                println!("Key not found"); 
+            }
+            res
+        }
+    }
+}
